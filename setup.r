4ced72fcf43ddf43d047ec9b4d4c7f184c2100e6
Ryan Batchelor <ryanbatchelor87@gmail.com>
2024-08-14 08:28:30 +1000

    tidy bugs from fresh r session

--- setup.r
+++ setup.r
@@ -13,11 +13,17 @@ library(forcats)
 library(plotly)
 library(glue)
 library(patchwork)
+library(rvest)
 
 library(shiny)
 
 
 
+
+
+if(!dir.exists("raw-data/generator_information/")){
+  dir.create("raw-data/generator_information/")
+}
 
 
 
